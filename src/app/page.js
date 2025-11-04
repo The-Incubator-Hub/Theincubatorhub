c71d31498ed9669d6e8e6fe4c818a235de4db2ee
Fiyinfoluwa <enisfiyinfoluwa@gmail.com>
2025-11-04 18:40:59 +0100

    Grouped components, adjusted  file structure and imports for landing-page-components

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,12 @@
-import HeroSection from "@/components/HeroSection"
-import AboutIncubator from "@/components/AboutIncubator"
-import TrustedOrganizations from "@/components/TrustedOrganizations"
-import WhyIncubator from "@/components/WhyIncubator"
-import Statistics from "@/components/Statistics"
-import BootcampsCarousel from "@/components/BootcampsCarousel"
-import CTABanner from "@/components/CtaBanner"
-import TestimonialCarousel from "@/components/TestimonialCarousel"
-import PressReleases from "@/components/PressReleases"
+import HeroSection from "@/components/landing-page-components/HeroSection"
+import AboutIncubator from "@/components/landing-page-components/AboutIncubator"
+import TrustedOrganizations from "@/components/landing-page-components/TrustedOrganizations"
+import WhyIncubator from "@/components/landing-page-components/WhyIncubator"
+import Statistics from "@/components/landing-page-components/Statistics"
+import BootcampsCarousel from "@/components/landing-page-components/BootcampsCarousel"
+import CTABanner from "@/components/landing-page-components/CtaBanner"
+import TestimonialCarousel from "@/components/landing-page-components/TestimonialCarousel"
+import PressReleases from "@/components/landing-page-components/PressReleases"
 export default function Home() {
   return (
     <main>
