28bf04e745b943057c73c31c2b5606dd130a25ac
Olalere Dahunsimi Joseph <olalerejoseph143@gmail.com>
2025-11-07 11:11:01 +0100

    New updates

--- src/app/blogdetails/page.js
+++ src/app/blogdetails/page.js
@@ -1,15 +1,11 @@
-import Navbar from "@/components/Navbar.js";
 import Article from "@/components/Article.js";
-import FooterR from "@/components/FooterR.js"
 import BlogPostsGrid from "@/components/post.js";
 
 export default function Page() {
     return (
       <div>
-        <Navbar />
         <Article />
         <BlogPostsGrid />
-        <FooterR />
       </div>
     );
   }
--- src/app/teams/page.js
+++ src/app/teams/page.js
@@ -1,19 +1,16 @@
-import Navbar from "@/components/Navbar.js";
 import Header from "@/components/Header.js";
 import TeamIntro from "@/components/TeamIntro.js";
 import Teamgrid from "@/components/Teamgrid.js";
 import JoinUs from "@/components/JoinUs.js"
-import FooterR from "@/components/FooterR.js"
 
 export default function Page() {
   return (
     <div>
-      <Navbar />
       <Header />
       <TeamIntro />
       <Teamgrid />
       <JoinUs />
-      <FooterR />
     </div>
   );
 }
+
